c42fb83843df491dcd1ef5bb6acdbeef04cd26c7
shweta19972211 <shwetachanagoudar@gmail.com>
2022-10-07 17:09:02 +0530

    first commit

---
+++ fsd/src/fsdjavaprograms/datatypesandveriables.java
@@ -0,0 +1,12 @@
+package fsdjavaprograms;
+
+public class datatypesandveriables {
+
+	public static void main(String[] args) {
+		String name= "Shweta";
+		int mobileNumber= 109908767;
+	     String phoneNumber= "9876543234";
+         System.out.println(name + "phone number" + phoneNumber);
+	}
+
+}
